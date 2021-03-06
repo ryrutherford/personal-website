f8216993a7c38c30bb8369f5e52de962679d03ee
Ry Rutherford <ry.rutherford99@gmail.com>
2021-03-06 18:53:57 -0500

    Make nav/header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return(
         <header>
-            Header here
+            <nav className="nav">
+                <Link to="/" className="nav-logo link">[ Ry Rutherford ]</Link>
+                <div className="nav-list">
+                    <Link to="/" className="nav-link link">About</Link>
+                    <p className="nav-separator">/</p>
+                    <Link to="/" className="nav-link link">Projects</Link>
+                    <p className="nav-separator">/</p>
+                    <Link to="/" className="nav-link link">Resume</Link>
+                    <p className="nav-separator">/</p>
+                    <Link to="/" className="nav-link link">Miscellaneous</Link>
+                </div>
+            </nav>
         </header>
     );
 }
