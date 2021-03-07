08da9a629567360d80b75865141f0bfa74c2b636
Ry Rutherford <ry.rutherford99@gmail.com>
2021-03-07 00:25:00 -0500

    Finish home page and footer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Typical from "react-typical";
+import RyImage from "../images/RY.jpg";
 
 const Home = () => {
     return(
         <section className="home">
             <div className="home-box">
-                <p className="home-intro">
-                    Welcome to my website!
-                </p>
+                <div className="home-intro">
+                    <img className="home-img" src={RyImage} alt="Ry Rutherford"/>
+                    <p className="home-type"><Typical loop={Infinity} wrapper="b" steps={
+                        ["developer", 1000, "student", 1000, "music producer", 1000, "motorcyclist", 1000, "powerlifter", 1000]}/></p>
+                </div>
             </div>
         </section>
     );
